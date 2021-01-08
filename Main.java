eca5f5121dad3e5742ee0eca8441ee010f632705
Lada Fedorova <lada12111986@gmail.com>
2021-01-08 19:21:28 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1525;
+        int mile = 20;
+
+        int totalMile = amount / mile;
+        System.out.println(totalMile);
+    }
+}
